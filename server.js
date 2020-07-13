7e1a6f17bda3d86a230999e52d25bf1fdccac589
Jordan Walker <jwalker117@gmail.com>
2020-07-13 14:19:53 -0500

    modulated the register route

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+//Add controllers to endpoints
+const register = require('./controllers/register');
+
 //Connect to the pg database using knex
 const db= knex({
     client: 'pg',
@@ -67,38 +70,8 @@ app.post('/signin', (req, res) => {
     .catch(error => res.status(400).json('wrong credentials'));
 });
 
-app.post('/register', (req, res) => {
-    const{ email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-    //Create a transaction using knex
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-            .returning('*')
-            .insert({
-                email: loginEmail[0],
-                name: name,
-                joined: new Date()
-            })
-            .then(user => {
-                res.json(user[0]);
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch(error => {
-        //don't return the error so hackers can 
-        //find out what went wrong on your db
-        res.status(400).json('unable to register');
-    })       
-});
+//Using dependency injection
+app.post('/register',(req, res) => {register.handleRegister(req, res, db, bcrypt)});
 
 
 // Put routes
