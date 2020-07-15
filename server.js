dcf65d6ed70ff633cf5b909c0d4b530fa1b47af0
Jordan Walker <jwalker117@gmail.com>
2020-07-15 11:01:36 -0500

    updated database connection

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db= knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-asymmetrical-51864',
       user : 'jswalk117',
       password : '',
       database : 'smart-brain'
